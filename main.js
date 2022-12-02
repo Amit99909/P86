b2d66b78101ae0ac2fcfb7617eda81abc7e86683
Amit99909 <110826540+Amit99909@users.noreply.github.com>
2022-12-02 19:12:26 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var canvas = new fabric.Canvas('myCanvas');
 
 function new_image()
 {
-	
+src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpCnC7wzjtGcF_3BBE5pOzpfAJZEYmdYjNIdIg8Ak&s"	
 	
 }
 
